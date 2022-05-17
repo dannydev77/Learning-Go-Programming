64b8712b22aee836acf42cb6b03575dadb3ae58e
danny dev <devinfo797@gmail.com>
2022-05-17 20:53:46 +0300

    variable declaration formats: edited

--- 1.HelloWorld/main.go
+++ 1.HelloWorld/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+// method 4
+// Declaring at the package level
+// you have to use the full declaration syntax
+// var a int = 42
+
+// method 5
+// using a block of variables that are declared together
+// var (
+// 	studentName    string = "john doe"
+// 	subject        string = "Informational sciences"
+// 	years          int    = 4
+// 	totalSemesters int    = 8
+// )
+
+var a int = 44
 
 func main() {
-	fmt.Println("Hello and welcome to the first program in Go")
+
+	// method 3
+	// a := 55
+
+	var a int = 98
+	fmt.Println("===================================")
+	fmt.Println(a)
+	fmt.Println("===================================")
 }
